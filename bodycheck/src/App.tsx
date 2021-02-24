5057a0244158a0ee03d226d5152f3475799006ea
Jady Rufino <jadyrufa@gmail.com>
2021-02-24 20:21:53 -0300

    create first component

--- bodycheck/src/App.tsx
+++ bodycheck/src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Button } from './components/Button';
+import { ExperienceBar } from './components/ExperienceBar';
 
 import './style/global.css';
 
 function App() {
   return (
-    <div>
-      <Button color="green">
-        Botão 1
-      </Button>
-      <Button color="blue">
-        Botão 2
-      </Button>
-      <Button color="red">
-        Botão 3
-      </Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
